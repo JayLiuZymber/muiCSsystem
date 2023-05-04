d54bcffd91742b687be3771263c86e6af2d5e29f
Jay <jayliu@zymber.tech>
2023-05-04 09:07:45 +0800

    fix ERROR in ./src/index.js 18:33-41 export 'default' (imported as 'Provider') was not found in 'react-redux' (possible exports: Provider, ReactReduxContext, batch, connect, createDispatchHook, createSelectorHook, createStoreHook, shallowEqual, useDispatch, useSelector, useStore)
    fix ERROR in ./src/index.js 19:11-16
    export 'default' (imported as 'store') was not found in './store/index' (possible exports: store)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Provider from 'react-redux';
-import store from './store/index';
+import { Provider } from 'react-redux';
+import { store } from './store/index';
 
 import App from './App';
 import Login from './views/Login';
