9a0847de280741f23d6eac1f7aea3e74cf0eca76
Nelson Hsu <nelsonhsu1027@gmail.com>
2023-05-09 11:17:16 +0800

    Can show the snackbar when login success in home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 import NewAssignedClients from './NewAssignedClients';
@@ -10,32 +8,47 @@ import ClientAD from './ClientAD';
 import ClientBlog from './ClientBlog';
 import ClientFlowerRank from './ClientFlowerRank';
 import ClientHeartRank from './ClientHeartRank';
+import Snackbar from '@mui/material/Snackbar';
+import { useLocation } from "react-router-dom";
 
-export default function Home () {
+
+export default function Home(props) {
+    const location = useLocation();
+
+    const [showSnackbar, setShowSnackbar] = React.useState(location.state?.showSnackbar ?? false);
+
     return (
         <div>
-        <Box sx={{ flexGrow: 1, m:2}}>
-            <Grid container spacing={2}>
-                <Grid display="flex" xm={12}>
-                    <NewAssignedClients></NewAssignedClients>
+            <Box sx={{ flexGrow: 1, m: 2 }}>
+                <Grid container spacing={2}>
+                    <Grid display="flex" xm={12}>
+                        <NewAssignedClients></NewAssignedClients>
+                    </Grid>
+                    <Grid display="flex" xm={12}>
+                        <AssignedClients></AssignedClients>
+                    </Grid>
+                    <Grid display="flex" xm={6}>
+                        <ClientAD></ClientAD>
+                    </Grid>
+                    <Grid display="flex" xm={6}>
+                        <ClientBlog></ClientBlog>
+                    </Grid>
+                    <Grid display="flex" xm={6}>
+                        <ClientFlowerRank></ClientFlowerRank>
+                    </Grid>
+                    <Grid display="flex" xm={6}>
+                        <ClientHeartRank></ClientHeartRank>
+                    </Grid>
                 </Grid>
-                <Grid display="flex" xm={12}>
-                    <AssignedClients></AssignedClients>
-                </Grid>
-                <Grid display="flex" xm={6}>
-                    <ClientAD></ClientAD>
-                </Grid>
-                <Grid display="flex" xm={6}>
-                    <ClientBlog></ClientBlog>
-                </Grid>
-                <Grid display="flex" xm={6}>
-                    <ClientFlowerRank></ClientFlowerRank>
-                </Grid>
-                <Grid display="flex" xm={6}>
-                    <ClientHeartRank></ClientHeartRank>
-                </Grid>
-            </Grid>
             </Box>
+
+            <Snackbar
+        open={showSnackbar}
+        onClose={() => setShowSnackbar(false)}
+        autoHideDuration={5000}
+        sx={{ width: '100%' }}
+        message="Login Success!"
+      />
         </div>
     )
 };
