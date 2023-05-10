e2e2e952a2e9d44cc4dba4695207e9b8c26b5066
Jay <jayliu@zymber.tech>
2023-05-10 18:19:25 +0800

    + LoginLogs.jsx content

--- src/views/LoginLogs.jsx
+++ src/views/LoginLogs.jsx
@@ -1,5 +1,98 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import { DataGrid } from '@mui/x-data-grid';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
-export default function LoginLogs() {
-    return <div>LoginLogs</div>
+// Status
+// Reason
+// Timestamp
+const columns = [
+  // { field: 'id', headerName: 'ID', width: 1 },
+  {
+    field: 'status', 
+    headerName: 'Status', 
+    width: 200,
+    // editable: true,
+    headerAlign: 'center',
+    // textCenter: true,
+  },
+  {
+    field: 'reason',
+    headerName: 'Reason',
+    width: 200,
+    // editable: true,
+    headerAlign: 'center',
+  },
+  {
+    field: 'timestamp',
+    headerName: 'Timestamp',
+    width: 200,
+    // editable: true,
+    headerAlign: 'center',
+  },
+];
+
+const rows = [
+  { id: 1, status: 'Success', reason: '-', timestamp: '2023-05-10 11:02:03'},
+  { id: 2, status: 'Success', reason: '-', timestamp: '2023-05-09 11:02:03'},
+  { id: 3, status: 'Success', reason: '-', timestamp: '2023-05-08 11:02:03'},
+  { id: 4, status: 'Success', reason: '-', timestamp: '2023-05-06 22:02:03'},
+  { id: 5, status: 'Success', reason: '-', timestamp: '2023-05-06 11:02:03'},
+  { id: 6, status: 'Failed', reason: "Wrong 'password'", timestamp:  '2023-05-05 11:02:03'},
+  { id: 7, status: 'Success', reason: '-', timestamp: '2023-05-03 11:02:03'},
+  { id: 8, status: 'Success', reason: '-', timestamp: '2023-05-02 11:02:03'},
+  { id: 9, status: 'Success', reason: '-', timestamp: '2023-05-01 11:02:03'},
+];
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  height: 45,
+  flexGrow: 1,
+}));
+
+
+export default function LoginLogs () {
+    const defaultProps = {
+    options: rows.reason,
+    getOptionLabel: (option) => option.status,
+    };
+    const flatProps = {
+    options: rows.map((option) => option.status),
+    };
+    const [value, setValue] = React.useState(null);
+    const count = 46;
+    
+  return (
+    <Box sx={{ width:'70vw', m:2 }}>
+      {/* <Box sx={{ height: 400, width: '70vw' }}> */}
+      <Grid container rowSpacing={1}>
+        <Grid display="flex" xs={6} sm={6}>
+          <h3>Login Logs ({count})</h3>
+        </Grid>
+      </Grid>
+      <DataGrid
+        
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        pageSizeOptions={[10, 20]}
+        // checkboxSelection
+        disableRowSelectionOnClick
+      />
+    {/* </Box> */}
+    </Box>
+  );
 };
